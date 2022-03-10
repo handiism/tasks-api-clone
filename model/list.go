d1a2ed72ce02be699dc6d38ce115beeb097483ec
handirachmawan <mail.handira@gmail.com>
2022-03-10 09:32:09 +0700

    fix: remove nullable field

--- model/list.go
+++ model/list.go
@@ -1,19 +1,18 @@
 package model
 
 import (
-	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type List struct {
-	ID        uint         `gorm:"type:SERIAL;->" json:"id" faker:"-"`
-	UserID    uuid.UUID    `gorm:"type:uuid;not null" json:"user_id" faker:"-"`
-	Title     string       `gorm:"size:255;not null" json:"title" faker:"word"`
-	CreatedAt time.Time    `gorm:"not null" json:"created_at" faker:"-"`
-	UpdatedAt sql.NullTime `gorm:"default:null" json:"updated_at" faker:"-"`
-	Tasks     []Task       `gorm:"foreignKey:ListID" json:"tasks" faker:"-"`
+	ID        uint      `json:"id" faker:"-"`
+	UserID    uuid.UUID `gorm:"type:uuid;not null" json:"user_id" faker:"-"`
+	Title     string    `gorm:"size:255;not null" json:"title" faker:"word"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at" faker:"-"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at" faker:"-"`
+	Tasks     []Task    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"tasks" faker:"-"`
 }
 
 func (List) TableName() string {
--- model/user.go
+++ model/user.go
@@ -1,21 +1,20 @@
 package model
 
 import (
-	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type User struct {
-	ID        uuid.UUID    `gorm:"type:uuid;default:gen_random_uuid()" json:"id" faker:"-"`
-	Name      string       `gorm:"size:255;not null" json:"name" faker:"name"`
-	Email     string       `gorm:"uniqueIndex;size:255;not null" json:"email" faker:"email"`
-	Password  string       `gorm:"size:255;not null" json:"-" faker:"password"`
-	CreatedAt time.Time    `gorm:"not null" json:"created_at" faker:"-"`
-	UpdatedAt sql.NullTime `gorm:"default:null" json:"updated_at" faker:"-"`
-	Token     string       `gorm:"-" json:"token,omitempty" faker:"-"`
-	Lists     []List       `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"list" faker:"-"`
+	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()" json:"id" faker:"-"`
+	Name      string    `gorm:"size:255;not null" json:"name" faker:"name"`
+	Email     string    `gorm:"uniqueIndex;size:255;not null" json:"email" faker:"email"`
+	Password  string    `gorm:"size:255;not null" json:"-" faker:"password"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at" faker:"-"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at" faker:"-"`
+	Token     string    `gorm:"-" json:"token,omitempty" faker:"-"`
+	Lists     []List    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"list" faker:"-"`
 }
 
 func (User) TableName() string {
