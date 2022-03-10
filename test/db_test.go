ff75f39ee6b352e139b5f904bc76d56a36852c99
handirachmawan <mail.handira@gmail.com>
2022-03-10 09:42:23 +0700

    test: separate migrate and faker

--- test/db_test.go
+++ test/db_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/bxcodec/faker/v3"
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var randUUID []uuid.UUID = []uuid.UUID{
@@ -21,147 +21,149 @@ var randUUID []uuid.UUID = []uuid.UUID{
 	uuid.New(), uuid.New(), uuid.New(), uuid.New(), uuid.New(),
 }
 
-func TestFillDB(t *testing.T) {
+func TestMigrate(t *testing.T) {
 	db := openDBConn()
 	defer closeDBConn(db)
 
-	if err := db.AutoMigrate(
+	err := db.AutoMigrate(
 		&model.User{}, &model.List{}, &model.Task{}, &model.Subtask{},
-	); err != nil {
-		panic(err)
-	}
+	)
+
+	require.Nil(t, err)
+}
+
+func TestFaker(t *testing.T) {
+	db := openDBConn()
+	defer closeDBConn(db)
 
 	err := faker.AddProvider("ref", func(v reflect.Value) (interface{}, error) {
 		return uint(1 + rand.Intn(20)), nil
 	})
 
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
-	for i := 0; i < 20; i++ {
-		var user model.User
-		err := faker.FakeData(&user)
-		assert.Nil(t, err)
+	t.Run("User", func(t *testing.T) {
+		for i := 0; i < 20; i++ {
+			var user model.User
+			err := faker.FakeData(&user)
+			require.Nil(t, err)
 
-		user.ID = randUUID[i]
-		user.CreatedAt = time.Now()
+			user.ID = randUUID[i]
+			user.CreatedAt = time.Now()
 
-		if rand.Intn(4) == i%4 {
-			user.UpdatedAt = sql.NullTime{
-				Time:  user.CreatedAt.AddDate(0, 0, i+1),
-				Valid: true,
+			if rand.Intn(4) == i%4 {
+				user.UpdatedAt = time.Now()
 			}
+
+			repo := repo.NewUser(db)
+
+			user, err = repo.Create(user)
+
+			require.Nil(t, err)
+			require.NotEmpty(t, user)
 		}
+	})
 
-		repo := repo.NewUser(db)
+	t.Run("List", func(t *testing.T) {
+		for i := 0; i < 20; i++ {
+			var list model.List
 
-		user, err = repo.Create(user)
+			err := faker.FakeData(&list)
 
-		assert.Nil(t, err)
-		assert.NotEmpty(t, user)
-	}
+			require.Nil(t, err)
 
-	for i := 0; i < 20; i++ {
-		var list model.List
+			list.UserID = randUUID[rand.Intn(20)]
+			list.CreatedAt = time.Now()
 
-		err := faker.FakeData(&list)
-
-		assert.Nil(t, err)
-
-		list.UserID = randUUID[rand.Intn(20)]
-		list.CreatedAt = time.Now()
-
-		if rand.Intn(4) == i%4 {
-			list.UpdatedAt = sql.NullTime{
-				Time:  list.CreatedAt.AddDate(0, 0, i+1),
-				Valid: true,
+			if rand.Intn(4) == i%4 {
+				list.UpdatedAt = time.Now()
 			}
+
+			repo := repo.NewList(db)
+
+			list, err = repo.Create(list)
+
+			if err != nil {
+				t.FailNow()
+			}
+
+			require.Nil(t, err)
+			require.NotEmpty(t, list)
 		}
+	})
 
-		repo := repo.NewList(db)
+	t.Run("Tasks", func(t *testing.T) {
+		for i := 0; i < 20; i++ {
+			var task model.Task
 
-		list, err = repo.Create(list)
+			if err := faker.FakeData(&task); err != nil {
+				panic(err)
+			}
 
-		assert.Nil(t, err)
-		assert.NotEmpty(t, list)
-	}
+			task.ID = uint(i + 1)
+			task.CreatedAt = time.Now()
 
-	for i := 0; i < 20; i++ {
-		var task model.Task
+			sentence, err := faker.GetLorem().Sentence(reflect.Value{})
 
-		if err := faker.FakeData(&task); err != nil {
-			panic(err)
-		}
+			require.Nil(t, err)
 
-		task.ID = uint(i + 1)
-		task.CreatedAt = time.Now()
-
-		sentence, err := faker.GetLorem().Sentence(reflect.Value{})
-
-		assert.Nil(t, err)
-
-		switch s := sentence.(type) {
-		case string:
-			if i%3 == 0 {
-				task.Detail = sql.NullString{
-					String: s,
-					Valid:  true,
+			switch s := sentence.(type) {
+			case string:
+				if i%3 == 0 {
+					task.Detail = sql.NullString{
+						String: s,
+						Valid:  true,
+					}
 				}
 			}
-		}
 
-		if rand.Intn(4) == i%4 {
-			task.UpdatedAt = sql.NullTime{
-				Time:  task.CreatedAt.AddDate(0, 0, i+1),
-				Valid: true,
+			if rand.Intn(4) == i%4 {
+				task.UpdatedAt = time.Now()
+				task.DueDate = sql.NullTime{
+					Time:  task.UpdatedAt.AddDate(0, 0, i+1),
+					Valid: true,
+				}
 			}
-		}
 
-		if !task.UpdatedAt.Valid {
-			task.DueDate = sql.NullTime{
-				Time:  task.UpdatedAt.Time.AddDate(0, 0, i+1),
-				Valid: true,
+			if rand.Intn(6) < 4 && task.DueDate.Valid {
+				task.IsDone = true
+			} else {
+				task.IsDone = false
 			}
+
+			repo := repo.NewTask(db)
+
+			task, err = repo.Create(task)
+
+			require.Nil(t, err)
+			require.NotEmpty(t, task)
 		}
+	})
 
-		if rand.Intn(6) < 4 && task.DueDate.Valid {
-			task.IsDone = true
-		} else {
-			task.IsDone = false
-		}
+	t.Run("Subtask", func(t *testing.T) {
+		for i := 0; i < 20; i++ {
+			var subtask model.Subtask
 
-		repo := repo.NewTask(db)
+			err := faker.FakeData(&subtask)
 
-		task, err = repo.Create(task)
+			require.Nil(t, err)
 
-		assert.Nil(t, err)
-		assert.NotEmpty(t, task)
-	}
+			subtask.ID = uint(i + 1)
+			subtask.CreatedAt = time.Now()
 
-	for i := 0; i < 20; i++ {
-		var subtask model.Subtask
-
-		err := faker.FakeData(&subtask)
-
-		assert.Nil(t, err)
-
-		subtask.ID = uint(i + 1)
-		subtask.CreatedAt = time.Now()
-
-		if rand.Intn(4) == i%4 {
-			subtask.UpdatedAt = sql.NullTime{
-				Time:  subtask.CreatedAt.AddDate(0, 0, i+1),
-				Valid: true,
+			if rand.Intn(4) == i%4 {
+				subtask.UpdatedAt = time.Now()
+				subtask.IsDone = true
+			} else {
+				subtask.IsDone = false
 			}
-			subtask.IsDone = true
-		} else {
-			subtask.IsDone = false
+
+			repo := repo.NewSubtask(db)
+
+			subtask, err = repo.Create(subtask)
+
+			require.Nil(t, err)
+			require.NotEmpty(t, subtask)
 		}
-
-		repo := repo.NewSubtask(db)
-
-		subtask, err = repo.Create(subtask)
-
-		assert.Nil(t, err)
-		assert.NotEmpty(t, subtask)
-	}
+	})
 }
