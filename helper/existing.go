f40d0a285abb9d89efe77064a599f8081cf0ce18
handirachmawan <mail.handira@gmail.com>
2022-03-10 09:22:10 +0700

    refactor: function name and strings panic

--- helper/existing.go
+++ helper/existing.go
@@ -2,17 +2,18 @@ package helper
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/google/uuid"
 )
 
-func ExistingUUID() uuid.UUID {
+func ExistingUserUUID() uuid.UUID {
 	GetEnv()
 
-	stringUUID := os.Getenv("EXISTING_UUID")
+	stringUUID := os.Getenv("USER_UUID")
 
 	if stringUUID == "" {
-		panic("value of exisiting_uuid is empty")
+		panic("value of user_uuid is empty")
 	}
 
 	uuid, err := uuid.Parse(stringUUID)
@@ -24,14 +25,50 @@ func ExistingUUID() uuid.UUID {
 	return uuid
 }
 
-func ExistingEmail() string {
+func ExistingUserEmail() string {
 	GetEnv()
 
-	email := os.Getenv("EXISTING_EMAIL")
-	
+	email := os.Getenv("USER_EMAIL")
+
 	if email == "" {
-		panic("value of existing_email is empty")
+		panic("value of user_email is empty")
 	}
 
 	return email
+}
+
+func ExistingListID() uint {
+	GetEnv()
+
+	id := os.Getenv("LIST_ID")
+
+	if id == "" {
+		panic("value of list_id is empty")
+	}
+
+	parsedID, err := strconv.Atoi(id)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return uint(parsedID)
+}
+
+func ExistingListUserID() uuid.UUID {
+	GetEnv()
+
+	stringUUID := os.Getenv("LIST_USER_UUID")
+
+	if stringUUID == "" {
+		panic(stringUUID)
+	}
+
+	uuid, err := uuid.Parse(stringUUID)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return uuid
 }
