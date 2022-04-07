191932289fd2557d58c16c44ef5b9ca0dde5d053
handirachmawan <mail.handira@gmail.com>
2022-04-07 14:27:09 +0700

    refactor: rm faker and add json omitempty to struct slices

--- model/task.go
+++ model/task.go
@@ -6,15 +6,15 @@ import (
 )
 
 type Task struct {
-	ID        uint           `json:"id" faker:"-"`
-	ListID    uint           `gorm:"not null" json:"list_id" faker:"ref"`
-	Name      string         `gorm:"size:255;not null" json:"name" faker:"word"`
-	Detail    sql.NullString `gorm:"size:255" json:"detail" faker:"-"`
-	DueDate   sql.NullTime   `gorm:"default:null" json:"due_date" faker:"-"`
-	IsDone    bool           `gorm:"default:false;not null" json:"is_done" faker:"-"`
-	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at" faker:"-"`
-	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at" faker:"-"`
-	Subtasks  []Subtask      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"subtask" faker:"-"`
+	ID        uint           `json:"id"`
+	ListID    uint           `gorm:"not null" json:"list_id"`
+	Name      string         `gorm:"size:255;not null" json:"name"`
+	Detail    sql.NullString `gorm:"size:255" json:"detail"`
+	DueDate   sql.NullTime   `gorm:"default:null" json:"due_date"`
+	IsDone    bool           `gorm:"default:false;not null" json:"is_done"`
+	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	Subtasks  []Subtask      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"subtask,omitempty"`
 }
 
 func (Task) TableName() string {
--- model/user.go
+++ model/user.go
@@ -7,14 +7,14 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()" json:"id" faker:"-"`
-	Name      string    `gorm:"size:255;not null" json:"name" faker:"name"`
-	Email     string    `gorm:"uniqueIndex;size:255;not null" json:"email" faker:"email"`
-	Password  string    `gorm:"size:255;not null" json:"-" faker:"password"`
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at" faker:"-"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at" faker:"-"`
-	Token     string    `gorm:"-" json:"token,omitempty" faker:"-"`
-	Lists     []List    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"list" faker:"-"`
+	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()" json:"id"`
+	Name      string    `gorm:"size:255;not null" json:"name"`
+	Email     string    `gorm:"uniqueIndex;size:255;not null" json:"email"`
+	Password  string    `gorm:"size:255;not null" json:"-"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	Token     string    `gorm:"-" json:"token,omitempty"`
+	Lists     []List    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"list,omitempty"`
 }
 
 func (User) TableName() string {
