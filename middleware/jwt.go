a38b89db847324977dc857ab9f90ec7b7a48ce02
handirachmawan <mail.handira@gmail.com>
2022-04-08 00:35:47 +0700

    feat: add jwt middleware

---
+++ middleware/jwt.go
@@ -0,0 +1,67 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	dtores "github.com/handirachmawan/tasks-api-clone/dto/response"
+	"github.com/handirachmawan/tasks-api-clone/repo"
+	"github.com/handirachmawan/tasks-api-clone/service"
+)
+
+func AuthorizeJWT(userRepo repo.UserRepo) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if !strings.Contains(authHeader, "Bearer") {
+			response := dtores.NewResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		var tokenStr string
+		arr := strings.Split(authHeader, " ")
+		jwtService := service.NewJWTService()
+
+		if len(arr) == 2 {
+			tokenStr = arr[1]
+		}
+
+		token, err := jwtService.Validate(tokenStr)
+
+		if err != nil {
+			response := dtores.NewResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		claims, ok := token.Claims.(*service.UUIDClaims)
+
+		if !ok || !token.Valid {
+			response := dtores.NewResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		uuid, err := uuid.Parse(claims.UUID)
+
+		if err != nil {
+			response := dtores.NewResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		user, err := userRepo.FindByUUID(uuid)
+		if err != nil {
+			response := dtores.NewResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		user, _ = userRepo.PreloadAll(user)
+
+		c.Set("user", user)
+	}
+}
