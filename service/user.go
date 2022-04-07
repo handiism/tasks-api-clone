0c3752340a486f51b489ee5545016f3bf5a88ccf
handirachmawan <mail.handira@gmail.com>
2022-04-08 00:35:16 +0700

    feat: add user service

---
+++ service/user.go
@@ -0,0 +1,115 @@
+package service
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	dtoreq "github.com/handirachmawan/tasks-api-clone/dto/request"
+	"github.com/handirachmawan/tasks-api-clone/model"
+	"github.com/handirachmawan/tasks-api-clone/repo"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService interface {
+	Register(inp dtoreq.Register) (model.User, error)
+	Login(inp dtoreq.Login) (model.User, error)
+	IsEmailAvailable(inp dtoreq.EmailChecking) bool
+	Fetch(id string) (model.User, error)
+	Delete(id string) error
+}
+
+type userService struct {
+	userRepo repo.UserRepo
+}
+
+func NewUserService(userRepo repo.UserRepo) UserService {
+	return &userService{
+		userRepo: userRepo,
+	}
+}
+
+func (u *userService) Register(inp dtoreq.Register) (model.User, error) {
+	avail := u.IsEmailAvailable(dtoreq.EmailChecking{Email: inp.Email})
+	if !avail {
+		return model.User{}, errors.New("cannot use registered email")
+	}
+
+	pw, err := bcrypt.GenerateFromPassword([]byte(inp.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return model.User{}, err
+	}
+	
+	now := time.Now()
+	user := model.User{
+		Name:      inp.Name,
+		Email:     inp.Email,
+		Password:  string(pw),
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	user, err = u.userRepo.Store(user)
+	if err != nil {
+		return model.User{}, err
+	}
+
+	return user, nil
+}
+
+func (u *userService) Login(inp dtoreq.Login) (model.User, error) {
+	user, err := u.userRepo.FindByEmail(inp.Email)
+
+	if err != nil {
+		return model.User{}, errors.New("email not registered yet")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(inp.Password)); err != nil {
+		return model.User{}, err
+	} else {
+		
+		return user, nil
+	}
+}
+
+func (u *userService) IsEmailAvailable(inp dtoreq.EmailChecking) bool {
+	if _, err := u.userRepo.FindByEmail(inp.Email); err != nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (u *userService) Fetch(id string) (model.User, error) {
+	uuid, err := uuid.Parse(id)
+
+	if err != nil {
+		return model.User{}, err
+	}
+
+	user, err := u.userRepo.FindByUUID(uuid)
+	if err != nil {
+		return model.User{}, err
+	}
+
+	return user, nil
+}
+
+func (u *userService) Delete(id string) error {
+	uuid, err := uuid.Parse(id)
+	if err != nil {
+		return err
+	}
+
+	user, err := u.userRepo.FindByUUID(uuid)
+	if err != nil {
+		return err
+	}
+
+	err = u.userRepo.Delete(user)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
