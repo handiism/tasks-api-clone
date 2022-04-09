d09ac0d5c1f37a6fa371ff4198417807c260e1c1
handirachmawan <mail.handira@gmail.com>
2022-04-09 15:46:26 +0700

    feat: add list middleware

---
+++ middleware/list.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	dtores "github.com/handiism/tasks-api-clone/dto/response"
+)
+
+func AuthorizeUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if _, exists := c.Get("user"); !exists {
+			response := dtores.NewErrReponse(
+				http.StatusBadRequest,
+				"Bad Request",
+				errors.New("failed to fetch user after jwt middleware"),
+			)
+			c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		}
+	}
+}
