c260b453c4cac91198fff5899f4e89ef4c2c633f
handiism <mail.handira@gmail.com>
2022-04-29 16:04:10 +0700

    feat: add http body request of update/create task and subtask

--- dto/request/save.go
+++ dto/request/save.go
@@ -1,5 +1,7 @@
 package request
 
+import "database/sql"
+
 type SaveUser struct {
 	Name     string `binding:"required"                json:"name"`
 	Email    string `binding:"required,email"          json:"email"`
@@ -8,4 +10,16 @@ type SaveUser struct {
 
 type SaveList struct {
 	Title string `binding:"required,min=1" json:"title"`
+}
+
+type SaveTask struct {
+	Name    string         `binding:"required"         json:"name"`
+	Detail  sql.NullString `binding:"min=0"            json:"detail,omitempty"`
+	DueDate sql.NullTime   `binding:"datetime"         json:"due_date,omitempty"`
+	IsDone  bool           `binding:"required,boolean" json:"is_done"`
+}
+
+type SaveSubtask struct {
+	Name   string `binding:"required"         json:"name"`
+	IsDone bool   `binding:"required,boolean" json:"is_done"`
 }
